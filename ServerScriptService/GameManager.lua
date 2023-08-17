c3e131deeb0797f92b5870fcd1ce43baf4a17adc
Ramazan  TOY <ramcotoy06@gmail.com>
2023-08-17 13:35:45 +0300

    kill points added

--- ServerScriptService/GameManager.lua
+++ ServerScriptService/GameManager.lua
@@ -16,4 +16,35 @@ local function addBoard(player)
 	
 end
 
-game.Players.PlayerAdded:Connect(addBoard);
+--game.Players.PlayerAdded:Connect(addBoard);
+
+game.Players.PlayerAdded:Connect(function(player)
+	addBoard(player)
+	player.CharacterAdded:Connect(function(char)
+		local hum=char:WaitForChild("Humanoid");
+		hum.Died:Connect(function()
+			hum:UnequipTools();
+			local tag=hum:FindFirstChild("creator");
+			if tag and tag.Value then
+				local ePlayer=tag.Value;
+				if ePlayer:FindFirstChild("leaderstats") then
+					local killsStat = ePlayer.leaderstats:FindFirstChild("Kills")
+					local pointsStat = ePlayer.leaderstats:FindFirstChild("Points")
+
+					if killsStat and pointsStat then
+						killsStat.Value += 1
+						pointsStat.Value += 50
+					end
+				end
+			end
+			
+			if player:FindFirstChild("leaderstats") then
+				local deathsStat = player.leaderstats:FindFirstChild("Deaths")
+
+				if deathsStat then
+					deathsStat.Value += 1
+				end
+			end
+		end)
+	end)
+end)
