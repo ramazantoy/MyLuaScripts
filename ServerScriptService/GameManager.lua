8aae0ee7b77de75e86b43ac4c08c19186d31774a
Ramazan  TOY <ramcotoy06@gmail.com>
2023-08-17 15:46:15 +0300

    save data added

--- ServerScriptService/GameManager.lua
+++ ServerScriptService/GameManager.lua
@@ -1,3 +1,69 @@
+
+local dataStoreService=game:GetService("DataStoreService");
+
+local ds=dataStoreService:GetDataStore("MyData");
+
+
+local function InitalizePlayerData(player)
+	local data=nil;
+	local succes,msg=pcall(function()
+		
+		data=ds:GetAsync(player.UserId);
+		
+	end)
+	
+	if not succes then
+		
+		player:Kick("Datastore unreachable, pls try again ", msg);
+		
+	end
+	
+	if data then
+		local leaderStats=player:FindFirstChild("leaderstats");
+		leaderStats.Points.Value=data.Points;
+		leaderStats.Kills.Value=data.Kills;
+		leaderStats.Deaths.Value=data.Deaths;
+	else
+		data={Points=0,Deaths=0,Kills=0};
+		--print("The First Time Player");
+		-- maybe do remove event welcome screen
+		
+	end
+	ds:SetAsync(player.UserId,data);
+	-- data is false for clear data
+	
+	
+end
+
+local function SavePlayerData(player)
+	
+	local data={};
+	local leaderStats=player:FindFirstChild("leaderstats");
+	data.Points=leaderStats.Points.Value;
+	data.Kills=leaderStats.Kills.Value;
+	data.Deaths=leaderStats.Deaths.Value
+	ds:SetAsync(player.UserId,data);
+	
+	
+end
+
+local function SaveLoop()
+	
+	while wait(5) do
+		local players=game.Players:GetChildren();
+		for i,player in pairs(players) do
+			local board=player:FindFirstChild("leaderstats");
+			
+			if board then
+				SavePlayerData(player);
+				print("Save loop",player.Name);
+				wait();
+			end
+		end
+	end
+end
+	
+
 local function addBoard(player)
 	
 	local board=Instance.new("Folder",player);
@@ -16,8 +82,6 @@ local function addBoard(player)
 	
 end
 
---game.Players.PlayerAdded:Connect(addBoard);
-
 local function ExcludeAccessory(part)
 	if part:IsA("Accessory") then
 		for i, v in pairs(part:GetChildren()) do
@@ -33,6 +97,7 @@ end
 
 game.Players.PlayerAdded:Connect(function(player)
 	addBoard(player)
+	InitalizePlayerData(player)
 	player.CharacterAdded:Connect(function(char)
 		local hum=char:WaitForChild("Humanoid");
 		hum.Died:Connect(function()
@@ -53,7 +118,7 @@ game.Players.PlayerAdded:Connect(function(player)
 			
 			if player:FindFirstChild("leaderstats") then
 				local deathsStat = player.leaderstats:FindFirstChild("Deaths")
-
+	
 				if deathsStat then
 					deathsStat.Value += 1
 				end
@@ -72,3 +137,8 @@ game.Players.PlayerAdded:Connect(function(player)
 		end)
 	end)
 end)
+
+spawn(SaveLoop); -- spawn thread for save loop;
+
+game.Players.PlayerRemoving:Connect(SavePlayerData); -- on leave saving;
+
