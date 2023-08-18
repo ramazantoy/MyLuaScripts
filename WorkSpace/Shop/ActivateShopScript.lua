4a65dd2a1002a0e8c74edf163852c6595eae7803
Ramazan  TOY <ramcotoy06@gmail.com>
2023-08-18 14:34:24 +0300

    Update ActivateShopScript.lua

--- WorkSpace/Shop/ActivateShopScript.lua
+++ WorkSpace/Shop/ActivateShopScript.lua
@@ -12,3 +12,17 @@ click.MouseClick:Connect(function(player)
 	
 	
 end)
+
+shopRe.OnServerEvent:Connect(function(player,itemName,itemPrice)
+	
+	local item=rs:WaitForChild(itemName);
+	
+	if item then
+		local itemClone=item:Clone();
+		itemClone.Parent=player.Backpack;
+		local price=tonumber(itemPrice);
+		if(price<=2000 and price>=500) then
+			player.leaderstats.Points.Value-=price;
+		end
+	end
+end)
