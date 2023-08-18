dd1ab6cc15248290259ea7d2cab03a5a4c837e34
Ramazan  TOY <ramcotoy06@gmail.com>
2023-08-18 15:50:04 +0300

    Create EquipStorageUtils.lua

---
+++ ServerScriptService/EquipStorageUtils.lua
@@ -0,0 +1,37 @@
+local equipUtils = {};
+
+
+--data table that has the equipment info for each player
+local equipmentTbl={};
+
+
+
+
+--called when purchased
+
+function equipUtils:addItemToPlayersInventory(player,itemInfoTbl)
+	
+end
+
+--called when players enter the game
+
+function equipUtils:updatePlayerWithDbInfo(player,data)
+	
+	
+end
+
+--equip player, data is already in table, but player doesn't have it 
+
+function equipUtils:equipPlayer(player)
+	
+	
+end
+
+--when players removed from game, remove from into table
+
+function equipUtils:removePlayer(player)
+	
+	
+end
+
+return equipUtils;
