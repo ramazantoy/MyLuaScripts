e84ccb7a668d64bab5935351de48e38d962783a5
Ramazan  TOY <ramcotoy06@gmail.com>
2023-08-21 14:46:51 +0300

    npc mov on maze, like navmesh agent

---
+++ WorkSpace/NpcMovement/NpcMovementOnMaze.lua
@@ -0,0 +1,49 @@
+local character=script.Parent;
+
+
+local targetObject=workspace.SimpleMaze.Target;
+
+local pathFindService=game:GetService("PathfindingService");
+
+local path=pathFindService:CreatePath();
+
+local wayPointIndex=1;
+
+local wayPointsTable={};
+
+local humanoidObj=character:WaitForChild("Humanoid");
+
+local humanoidRootPartObj=character:WaitForChild("HumanoidRootPart");
+
+local function FallowThePath(goal)
+	
+	path:ComputeAsync(humanoidRootPartObj.Position,goal.Position);
+	wayPointsTable={};
+	
+	if path.Status==Enum.PathStatus.Success then
+		
+		wayPointsTable=path:GetWaypoints();
+		wayPointIndex=1;
+		humanoidObj:MoveTo(wayPointsTable[wayPointIndex].Position);
+	end
+	
+end
+
+humanoidObj.MoveToFinished:Connect(function(reached)
+	
+	if(reached and wayPointIndex<#wayPointsTable) then
+		wayPointIndex+=1;
+		if wayPointsTable[wayPointIndex].Action==Enum.PathWaypointAction.Jump then
+			
+			humanoidObj.Jump=true;
+		end
+		humanoidObj:MoveTo(wayPointsTable[wayPointIndex].Position);
+	
+	end
+	
+end)
+
+wait(5);
+
+FallowThePath(targetObject);
+
